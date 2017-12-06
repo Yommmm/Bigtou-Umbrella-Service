26d222e76bcdd86626ba2533018b7fecd80d8a19
yommmm <1529133840@qq.com>
2017-12-06 21:59:38 +0800

    first commit

---
+++ src/main/java/com/bigtou/umbrella/UmbrellaApplication.java
@@ -0,0 +1,12 @@
+package com.bigtou.umbrella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbrellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmbrellaApplication.class, args);
+	}
+}
