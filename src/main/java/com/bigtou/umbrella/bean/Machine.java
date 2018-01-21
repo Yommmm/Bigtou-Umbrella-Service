7bd3d5ea0c0558ff8dcc63e237517a6c422f5ab0
yommmm <1529133840@qq.com>
2018-01-21 14:37:30 +0800

    add

--- src/main/java/com/bigtou/umbrella/bean/Machine.java
+++ src/main/java/com/bigtou/umbrella/bean/Machine.java
@@ -2,4 +2,54 @@ package com.bigtou.umbrella.bean;
 
 public class Machine {
 
+	private String machineId;
+	
+	private String machineName;
+	
+	private String machineIP;
+	
+	private String machineAddress;
+	
+	private String SJFlag;
+
+	public String getMachineId() {
+		return machineId;
+	}
+
+	public void setMachineId(String machineId) {
+		this.machineId = machineId;
+	}
+
+	public String getMachineName() {
+		return machineName;
+	}
+
+	public void setMachineName(String machineName) {
+		this.machineName = machineName;
+	}
+
+	public String getMachineIP() {
+		return machineIP;
+	}
+
+	public void setMachineIP(String machineIP) {
+		this.machineIP = machineIP;
+	}
+
+	public String getMachineAddress() {
+		return machineAddress;
+	}
+
+	public void setMachineAddress(String machineAddress) {
+		this.machineAddress = machineAddress;
+	}
+
+	public String getSJFlag() {
+		return SJFlag;
+	}
+
+	public void setSJFlag(String sJFlag) {
+		SJFlag = sJFlag;
+	}
+	
 }
--- src/main/java/com/bigtou/umbrella/bean/Umbrella.java
+++ src/main/java/com/bigtou/umbrella/bean/Umbrella.java
@@ -2,4 +2,34 @@ package com.bigtou.umbrella.bean;
 
 public class Umbrella {
 
+	private String umbrellaId;
+	
+	private String umbrellaName;
+	
+	private String CSFlag;
+
+	public String getUmbrellaId() {
+		return umbrellaId;
+	}
+
+	public void setUmbrellaId(String umbrellaId) {
+		this.umbrellaId = umbrellaId;
+	}
+
+	public String getUmbrellaName() {
+		return umbrellaName;
+	}
+
+	public void setUmbrellaName(String umbrellaName) {
+		this.umbrellaName = umbrellaName;
+	}
+
+	public String getCSFlag() {
+		return CSFlag;
+	}
+
+	public void setCSFlag(String cSFlag) {
+		CSFlag = cSFlag;
+	}
+	
 }
